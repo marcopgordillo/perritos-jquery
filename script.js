f80070ce5855d6904df0160201763a63517b8154
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2020-08-06 17:46:27 -0500

    Add input number

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 $.noConflict();
-jQuery(document).ready(function($) {
-  let weight = $("#weight");
+jQuery(document).ready(function ($) {
+  const weight = $("#weight");
+  const weightInput = $("#weight-input");
 
   $("#list>li").click(function () {
     const id = $(this).data("id");
     activeClass(id * 10);
     weight.val(id * 10);
+    weightInput.val(id * 10);
   });
 
   weight.change(function () {
     const val = $(this).val();
+    weightInput.val(val);
     console.log(val);
+    activeClass(val);
+  });
+
+  weightInput.change(function () {
+    const val = $(this).val();
+    weight.val(val);
     activeClass(val);
   });
 
   function activeClass(val) {
     $("#list>li").removeClass("text-danger");
     $(`#dog-${Math.floor(val / 10)}`).addClass("text-danger");
-  }  
+  }
 });
