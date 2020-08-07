3174665e81b79b3f286314184bd34bfa2ccffb2b
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2020-08-07 14:03:09 -0500

    Add bootstrap-icons sprite fail

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 $.noConflict();
 jQuery(document).ready(function ($) {
-  const inputRange = $("#weight");
-  const inputNumber = $("#weight-input");
-  const dogs = $(".js-dog");
+  const inputRange = $("#weight"),
+    inputNumber = $("#weight-input"),
+    dogs = $(".js-dog");
+
   let oldVal = 0;
 
   // click dogs
   dogs.click(function () {
-    const id = $(this).data("id");
-    oldVal = id * 10;
+    oldVal = $(this).data("id") * 10;
     activeClass(oldVal);
     inputRange.val(oldVal);
     inputNumber.val(oldVal);
@@ -16,7 +16,7 @@ jQuery(document).ready(function ($) {
 
   // Change input range
   inputRange.change(function () {
-    const newVal = $(this).val();
+    const newVal = +$(this).val();
     inputNumber.val(newVal);
     if (!isEqual(newVal)) {
       oldVal = newVal;
@@ -26,7 +26,7 @@ jQuery(document).ready(function ($) {
 
   // Change input number
   inputNumber.change(function () {
-    const newVal = $(this).val();
+    const newVal = +$(this).val();
     inputRange.val(newVal);
     if (!isEqual(newVal)) {
       oldVal = newVal;
@@ -35,11 +35,10 @@ jQuery(document).ready(function ($) {
   });
 
   function isEqual(newVal) {
-    return Math.floor(oldVal / 10) === +Math.floor(newVal / 10);
+    return Math.floor(oldVal / 10) === Math.floor(newVal / 10);
   }
 
   function activeClass(val) {
-    console.log(val);
     dogs.removeClass("text-danger");
     $(`#dog-${Math.floor(val / 10)}`).addClass("text-danger");
   }
